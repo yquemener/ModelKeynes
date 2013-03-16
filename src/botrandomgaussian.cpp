ed7938536613ddf12470cce5036ce8ba061c963d
Yves Quemener <Iv@laptop>
2013-03-16 22:25:40 +0100

    Random bot finished

--- src/botrandomgaussian.cpp
+++ src/botrandomgaussian.cpp
@@ -8,7 +8,7 @@ using std::list;
 namespace Econ
 {
 
-BotRandomGaussian::BotRandomGaussian(IndustrialNode* home, float sigma=1.0)
+BotRandomGaussian::BotRandomGaussian(IndustrialNode* home, float sigma=0.1f)
   : Bot(home)
 {
   m_sigma = sigma;
@@ -20,16 +20,21 @@ void BotRandomGaussian::updateInfo()
 
 list<order> BotRandomGaussian::makeTradingDecisions()
 {
+  list<order> ret;
+  // Buy orders
   // ppu = price per unit
   float total_ppu = 0.0f;
-  std::normal_distribution<double> distribution(0.0, m_sigma);
-
+  std::normal_distribution<double> distribution(1.0, m_sigma);
+  std::map<good_t, float> price_to_pay;
   for(auto it = m_home->m_unitInput.begin();
       it != m_home->m_unitInput.end();
       it++)
   {
     pair<good_t, float> ui = *it;
     float uprice = ui.second * m_home->m_InputMarkets[ui.first]->currentPrice();
+    float rand_var = distribution(s_rng());
+    uprice *= rand_var;
+    price_to_pay[ui.first] = uprice;
     total_ppu += uprice;
   }
 
@@ -39,8 +44,30 @@ list<order> BotRandomGaussian::makeTradingDecisions()
       it != m_home->m_unitInput.end();
       it++)
   {
-    sd
+    order o;
+    pair<good_t, float> ui = *it;
+    o.author = m_id;
+    o.type = BUY;
+    o.price = price_to_pay[ui.first];
+    o.volume = ui.second * units_to_buy;
+    ret.push_back(o);;
   }
+
+  for(auto it = m_home->m_OutputMarkets.begin();
+      it!=m_home->m_OutputMarkets.end();
+      it++)
+  {
+    good_t g = it->first;
+    Market *m = it->second;
+    float rand_var = distribution(s_rng());
+    order o;
+    o.price = m->currentPrice() * rand_var;
+    o.volume = this->stock[g];
+    o.author = this->m_id;
+    o.type = SELL;
+  }
+
+  return ret;
 }
 
 float BotRandomGaussian::makeProcessingDecisions()
